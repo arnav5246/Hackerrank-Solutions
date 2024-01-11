4535625dcdefb686642257de94c6ce1a153f56ad
Arnav Jain <arnavjain5246@gmail.com>
2024-01-11 21:55:52 +0530

    repo updated

---
+++ C++ Language/Inherited Code.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <sstream>
+#include <exception>
+using namespace std;
+
+/* Define the exception here */
+struct BadLengthException : exception 
+{
+  string s;
+  BadLengthException(int n) : s(to_string(n)) {}
+  const char *what() const noexcept override 
+  {
+    return s.c_str();
+  }
+};
