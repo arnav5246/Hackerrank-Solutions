8a4ac152d9d1778bfe30f2235fb5ca3cd15b0816
Arnav Jain <arnavjain5246@gmail.com>
2024-02-05 23:44:56 +0530

    new codes added

---
+++ Python/String Formatting.py
@@ -0,0 +1,5 @@
+def print_formatted(number):
+    # your code goes here
+    pad = number.bit_length()
+    for i in range(1,number+1):
+        print(format(i,'d').rjust(pad),format(i,'o').rjust(pad),format(i,'X').rjust(pad),format(i,'b').rjust(pad))
